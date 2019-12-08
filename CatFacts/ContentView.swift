33b1b4f8dd4b52ca739a09288167eec031acb887
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2019-12-08 15:45:10 +0200

    init

--- CatFacts/ContentView.swift
+++ CatFacts/ContentView.swift
@@ -10,18 +10,50 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var fact: String = ""
+    @State var allPages = [Item]()
+    
     var body: some View {
-        VStack {
-            Text(self.fact)
-                .font(.title)
-                .padding()
-            Button(action: {
-                self.loadPage()
-            }) {
-                Image(systemName: "arrow.2.circlepath")
+        TabView {
+            VStack {
+                Spacer()
+                Text(self.fact)
+                    .font(.title)
+                    .padding()
+                Spacer()
+                Button(action: {
+                    self.loadPage()
+                }) {
+                    Image(systemName: "arrow.2.circlepath")
+                        .font(.title)
+                }
+            .padding()
             }
-        }.onAppear(){
-            self.loadPage()
+            .tabItem({
+                Image(systemName: "leaf.arrow.circlepath")
+                    .resizable()
+                    .font(.title)
+            })
+            .onAppear(){
+                self.loadPage()
+                self.loadAllPages()
+            }
+            
+            VStack {
+                ScrollView {
+                    ForEach(self.allPages, id: \.id) {row in
+                        ItemView(item: row)
+                    }
+                }
+                Divider()
+            }
+            .tabItem {
+                Image(systemName: "list.bullet.below.rectangle")
+                    .resizable()
+                    .font(.title)
+            }
+            .onAppear() {
+                self.loadAllPages()
+            }
         }
     }
     
@@ -31,6 +63,32 @@ struct ContentView: View {
                 self.fact = page.text ?? "n/a"
             }
         }
+    }
+    
+    func loadAllPages() {
+        NetworkManager.fetchAllPages { pages in
+            DispatchQueue.main.async {
+                self.allPages.append(contentsOf: pages.all)
+            }
+        }
+    }
+}
+
+struct ItemView: View {
+    @State var item: Item
+    var body: some View {
+        VStack {
+            Text("\(item.text ?? "n/a")")
+                .foregroundColor(.primary)
+                .padding()
+            HStack {
+                Spacer()
+                Text("by \(item.user?.name?.first ?? "n/a") \(item.user?.name?.last ?? "n/a")")
+                    .foregroundColor(.secondary)
+                    .padding()
+            }
+        }
+        
     }
 }
 
--- CatFacts/NetworkManager.swift
+++ CatFacts/NetworkManager.swift
@@ -8,7 +8,8 @@
 
 import Foundation
 
-let endpointLink = "https://cat-fact.herokuapp.com/facts/random"
+let endpointLink = "https://cat-fact.herokuapp.com"
+let facts = "/facts"
 
 struct CatalogPage: Decodable {
     let used: Bool?
@@ -23,14 +24,51 @@ struct CatalogPage: Decodable {
     let __v: Int?
 }
 
+struct AllItems: Decodable {
+    let all: [Item]
+}
+
+struct Item: Decodable {
+    let id: UUID = UUID()
+    let _id: String?
+    let text: String?
+    let type: String?
+    let user: User?
+    let upvotes: Int?
+    let userUpvoted: String?
+}
+
+struct User: Decodable {
+    let _id: String?
+    let name: Name?
+}
+
+struct Name: Decodable {
+    let first: String?
+    let last: String?
+}
+
+
+
 class NetworkManager {
     
     static func fetchPage(_ path: String? = nil, completionHandler: @escaping (CatalogPage) -> Void) {
-        if let url = URL.init(string: path ?? endpointLink) {
+        if let url = URL.init(string: path ?? endpointLink + facts + "/random") {
             let task = URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
                 if let page: CatalogPage = try? JSONDecoder().decode(CatalogPage.self, from: data!) {
                     completionHandler(page)
                 }
+            })
+            task.resume()
+        }
+    }
+    
+    static func fetchAllPages(_ path: String? = nil, completionHandler: @escaping (AllItems) -> Void) {
+        if let url = URL.init(string: path ?? endpointLink + facts) {
+            let task = URLSession.shared.dataTask(with: url, completionHandler: { (data, response, error) in
+                if let page: AllItems = try? JSONDecoder().decode(AllItems.self, from: data!) {
+                    completionHandler(page)
+                } 
             })
             task.resume()
         }
